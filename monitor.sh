0535b66e7c2a7ab76fb56f6ba14b5223c03305ba
alex <alexxtreem@gmail.com>
2024-03-08 12:12:13 +0300

    Реализация перебора аргументов через case

--- monitor.sh
+++ monitor.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
-echo "Test 2"
+# Обрабатываем параметры и аргументы
+
+paramAndArgs () {
+    if [ $# == 0 ]
+    then
+        echo "Наберите 'monitor.sh --help' для вывода справки"
+    else
+    case $@ in
+        -p | --proc) echo "Комманда работы с директорией";;
+        -с | --cpu) echo "Комманда работы с процессором";;
+        -m | --memory) echo "Комманда работы с памятью";;
+        -d | --disk) "Комманда работы с диском";;
+        -n | --network) ;;
+        -la | --loadaverege) ;;
+        -k | --kill) ;;
+        -o | --output) ;;
+        -h | --help) echo "Помощь";;
+        *) echo "Не известная комманда $@ (наберите 'monitor.sh --help' для вывода справки)";;
+    esac
+    fi
+}
+
+
+paramAndArgs $@
